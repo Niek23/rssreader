d4e49ee9e57f967c22077660a46319a3bceedb26
Niek23 <nkt2397@gmail.com>
2021-07-08 00:06:03 +0200

    Add OpenAPI doc

--- rssreader/urls.py
+++ rssreader/urls.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="RSS Reader API",
+      default_version='v1',
+      description="Test description",
+      contact=openapi.Contact(email="nkt.gonchar@gmail.com"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path(r'admin/', admin.site.urls),
     path(r'auth/', include('users.urls')), 
-    path(r'api/', include('feed.urls'))
+    path(r'api/', include('feed.urls')),
+
+
+    # Documentation
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
